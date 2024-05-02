2aac8262191a9163f744fd4bcb61fd18fa11203d
KosikKa <129963099+KosikKa@users.noreply.github.com>
2024-05-02 20:33:59 +0300

    Add files via upload

--- push_request.js
+++ push_request.js
@@ -11,13 +11,31 @@ const config = {
 firebase.initializeApp(config);
 const messaging = firebase.messaging();
 
-async function getUserToken() {
-  try {
-    const token = await messaging.getToken();
-    document.getElementById("user_token").textContent = `Ваш токен: ${token}`;
-  } catch (error) {
-    console.error('Ошибка получения токена: ', error);
+// Добавление этого кода
+messaging.getToken().then((currentToken) => {
+  if (currentToken) {
+    console.log('Current token: ', currentToken);
+    document.getElementById("user_token").textContent = `Ваш токен: ${currentToken}`;
+  } else {
+    console.log('No token available.');
   }
-}
+}).catch((err) => {
+  console.log('An error occurred while retrieving token: ', err);
+});
+// Конец добавления
 
-getUserToken();
+document.getElementById("notification_subscribe").addEventListener("click", async () => {
+
+    await Notification.requestPermission();
+
+    if (Notification.permission === 'granted') {
+        await messaging.requestPermission();
+        const res = await navigator.permissions.query({ name: 'clipboard-write' });
+
+        if (res.state === "granted") {
+            const token = await messaging.getToken();
+            await navigator.clipboard.writeText(token);
+            document.getElementById("user_token").textContent = `Ваш токен: ${token}`;
+        }
+    }
+});
