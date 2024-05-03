e3d2c2d52ad1a61aa6787b733cba3794eeed3d56
KosikKa <129963099+KosikKa@users.noreply.github.com>
2024-05-03 14:49:36 +0300

    Add files via upload

--- push_request.js
+++ push_request.js
@@ -1,47 +1,37 @@
-const config = {
-  apiKey: "AIzaSyCtgaF_YzSrYXSOWIlS4qQPO3sIGwF0Gic",
-  authDomain: "maska-c8ab8.firebaseapp.com",
-  projectId: "maska-c8ab8",
-  storageBucket: "maska-c8ab8.appspot.com",
-  messagingSenderId: "183357275456",
-  appId: "1:183357275456:web:437c4fb81a04c168ddcea5",
-  measurementId: "G-9C7JF4Z33J"
-};
+// Инициализация OneSignal SDK
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+  OneSignal.init({
+    appId: "0036313c-0523-4f29-920f-6788372b9991",
+    autoRegister: false,
+    welcomeNotification: {
+      title: "Welcome!",
+      message: "Thanks for subscribing to our push notifications!"
+    }
+  });
 
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
+  // Получение токена устройства
+  OneSignal.getUserId(function(userId) {
+    // Сохранение токена в локальном хранилище
+    localStorage.setItem('onesignal_token', userId);
+  });
 
-// Запрашиваем разрешение на получение уведомлений при загрузке страницы
-messaging.requestPermission()
-.then(() => {
-  console.log('Разрешение на уведомления предоставлено.');
-  getToken();
-})
-.catch((error) => {
-  console.error('Не удалось получить разрешение на уведомления.', error);
+  // Обработчик события для кнопки подписки на push-уведомления
+  document.getElementById("notification_subscribe").addEventListener("click", function() {
+    OneSignal.push(function() {
+      OneSignal.registerForPushNotifications();
+    });
+  });
 });
 
-// Функция для получения токена устройства
-function getToken() {
-messaging.getToken().then((currentToken) => {
-  if (currentToken) {
-    console.log('Токен получен:', currentToken);
-    document.getElementById('user_token').textContent = currentToken;
-  } else {
-    console.log('Не удалось получить токен. Пользователь отклонил разрешение на уведомления.');
-  }
-}).catch((error) => {
-  console.error('Ошибка при получении токена: ', error);
-});
+// Получение токена из локального хранилища
+const onesignalToken = localStorage.getItem('onesignal_token');
+if (onesignalToken) {
+  // Вывод токена на экран
+  document.getElementById("user_token").textContent = onesignalToken;
 }
 
-// Обработчик события нажатия на кнопку "Подписаться на уведомления"
-document.getElementById("notification_subscribe").addEventListener("click", async () => {
-try {
-  const token = await messaging.getToken();
-  await navigator.clipboard.writeText(token);
-  console.log('Токен скопирован в буфер обмена:', token);
-} catch (error) {
-  console.error('Ошибка при копировании токена в буфер обмена:', error);
+// Регистрация сервис-воркера OneSignal
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/OneSignalSDKWorker.js');
 }
-});
