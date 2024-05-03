68df3fc979fbd9446704318e99ca219d81033ef9
KosikKa <129963099+KosikKa@users.noreply.github.com>
2024-05-03 12:42:29 +0300

    Add files via upload

--- push_request.js
+++ push_request.js
@@ -1,27 +1,24 @@
 const config = {
-    apiKey: "AIzaSyCtgaF_YzSrYXSOWIlS4qQPO3sIGwF0Gic",
-    authDomain: "maska-c8ab8.firebaseapp.com",
-    projectId: "maska-c8ab8",
-    storageBucket: "maska-c8ab8.appspot.com",
-    messagingSenderId: "183357275456",
-    appId: "1:183357275456:web:437c4fb81a04c168ddcea5",
-    measurementId: "G-9C7JF4Z33J"
+  apiKey: "AIzaSyDaWHKw97ue_C6PhNuHlIOjNTC3rlaPeQ8",
+  authDomain: "messa-a27d9.firebaseapp.com",
+  projectId: "messa-a27d9",
+  storageBucket: "messa-a27d9.appspot.com",
+  messagingSenderId: "291151785629",
+  appId: "1:291151785629:web:9fb2f3aa097082db6f2e74",
+  measurementId: "G-HSFBJRJFT6"
 };
 
 firebase.initializeApp(config);
 const messaging = firebase.messaging();
 
-document.getElementById("notification_subscribe").addEventListener("click", async () => {
-  if (Notification.permission === 'granted') {
-    await messaging.requestPermission();
-    const res = await navigator.permissions.query({ name: 'clipboard-write' });
+// Удалить слушатель события кнопки
+// document.getElementById("notification_subscribe").removeEventListener("click", async () => { ... });
 
-    if (res.state === "granted") {
-      const token = await messaging.getToken();
-      await navigator.clipboard.writeText(token);
-      document.getElementById("user_token").textContent = `Ваш токен: ${token}`;
-    }
-  } else {
-    console.log('Уведомления не разрешены.');
-  }
-});
+// Получить токен при загрузке страницы
+await messaging.requestPermission();
+const token = await messaging.getToken();
+console.log("Полученный токен:", token);
+
+// Отобразить токен
+const tokenDisplayElement = document.getElementById("token-display");
+tokenDisplayElement.textContent = "Ваш токен: " + token;
